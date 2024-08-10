2606900e1fc50afc2bd725fc16245ec172cb6822
vicente almeida <vicente.cra45@gmail.com>
2024-08-10 20:04:01 +0400

    Changing one million to entire project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import million from "million/compiler";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), million.vite({ auto: false })],
 })
